4259cd337dd36e21df53cf5aa7d11aab549dc892
vinaykumar586 <vinaykumar.gangadhar586@gmail.com>
2023-05-17 11:53:52 +0530

    changes added

--- src/Screens/CardComponent.js
+++ src/Screens/CardComponent.js
@@ -5,8 +5,16 @@ import './Card.css'
 import { getSubCategories } from '../Services/Categoryaction';
 function CardComponent({item,image,title}) {
 const dispatch = useDispatch();
+let cate=null
+// const CatgorySelect =(e)=> {
+//   if(cate!==null && cate.classList.contains("active-cate")){
+//     cate.classList.remove("active-cate")
+// }
+// e.target.classList.add("active-cate")
+// cate=e.target;
+// }
   return (
-    <div className='card' onClick={()=>{dispatch(getSubCategories(item.categoryId))}}>
+    <div className='card' onClick={(e)=>{dispatch(getSubCategories(item.categoryId)); CatgorySelect(e)}}>
         <img src={image} style={{width:"120px"}}/>
         <div className='category-name'>{title}</div>
     </div>
